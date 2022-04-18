24f7ebad622b5229f7301d33ad08ebc0fd841277
nykfeng <nykfengcode@gmail.com>
2022-04-17 21:55:50 -0400

    internal main page restructure

--- public/js/internal/index.js
+++ public/js/internal/index.js
@@ -1,8 +1,15 @@
 import modal from "./modal.js";
+import generateHTML from "./generateHTML.js";
 
 const addNewBrandBtn = document.querySelector(".brand-add");
 
+// listener for add a new brand
 addNewBrandBtn.addEventListener("click", function () {
   modal.open("Brand");
 });
 
+// make brand list
+function internalBrandList() {
+  // url route to pull from backend
+  // const brands = 
+} 
