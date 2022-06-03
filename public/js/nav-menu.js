26207a176889575cc6ead80741898fdf0d8a5f57
nykfeng <nykfengcode@gmail.com>
2022-06-02 23:14:22 -0400

    search bar responsive and allow dropdown open and close

--- public/js/nav-menu.js
+++ public/js/nav-menu.js
@@ -1,10 +1,15 @@
-// // DOM elements for working with hamburger button -------------------
+// DOM elements for working with hamburger button -------------------
 const hamburgerBtn = document.querySelector(".main-nav-hamburger-button");
 const mainNavEl = document.querySelector(".main-nav");
 const mainNavListEl = document.querySelector(".main-nav-list");
 
+// DOM elements for search bar
+const searchExpIconBtn = document.querySelector(".search-icon-to-expand");
+const searchBarEl = document.querySelector(".search-bar");
+
 // variable for hamburger button
 let hambugerSentinel = false;
+let searchIconSentinel = false;
 
 // hambuger button to show and close nav menu for smaller screens
 hamburgerBtn.addEventListener("click", function () {
@@ -18,5 +23,18 @@ hamburgerBtn.addEventListener("click", function () {
     mainNavEl.setAttribute("action", "close-dropdown");
     hambugerSentinel = false;
     hamburgerBtn.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  }
+});
+
+// search icon dropdown for search bar to show or disappear
+searchExpIconBtn.addEventListener("click", function () {
+  if (!searchIconSentinel) {
+    this.setAttribute("action", "open-dropdown");
+    searchIconSentinel = true;
+    this.innerHTML = `<i class="fa-solid fa-magnifying-glass-minus"></i>`;
+  } else {
+    this.setAttribute("action", "close-dropdown");
+    searchIconSentinel = false;
+    this.innerHTML = `<i class="fa-solid fa-magnifying-glass-plus"></i>`;
   }
 });
